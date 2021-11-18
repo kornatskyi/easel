06fdbda52bde916521e0c79973132f65528e107e
Bohdan <bohdan.kornatskyi@gmail.com>
2021-11-17 21:04:02 -0800

    added color picker

---
+++ app/canvas/components/ColorPicker.tsx
@@ -0,0 +1,29 @@
+import React, { useCallback, useRef, useState } from "react"
+import { HexColorPicker, HexColorInput } from "react-colorful"
+import useClickOutside from "../utils/useClickOutside"
+import styles from "../styles/colorpicker.module.scss"
+
+export const ColorPicker = (props) => {
+  const { setStroke, defaultColor } = props
+
+  const popover = React.useRef() as React.MutableRefObject<HTMLInputElement>
+  const [isOpen, toggle] = useState(false)
+
+  const close = useCallback(() => toggle(false), [])
+  useClickOutside(popover, close)
+  return (
+    <div className={styles.picker}>
+      <div
+        className={styles.swatch}
+        style={{ backgroundColor: defaultColor }}
+        onClick={() => toggle(true)}
+      />
+
+      {isOpen && (
+        <div className={styles.popover} ref={popover}>
+          <HexColorPicker color={defaultColor} onChange={setStroke} />
+        </div>
+      )}
+    </div>
+  )
+}
