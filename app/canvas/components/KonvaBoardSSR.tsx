bd6bedab977ac1658ac1838a037d6b5ab2c0c92f
Bohdan <bohdan.kornatskyi@gmail.com>
2021-11-17 16:14:39 -0800

    basic toolbar

--- app/canvas/components/KonvaBoardSSR.tsx
+++ app/canvas/components/KonvaBoardSSR.tsx
@@ -2,15 +2,57 @@ import React, { useRef, useState } from "react"
 import { Stage, Layer, Line, Text } from "react-konva"
 import styles from "../styles/konva-board.module.scss"
 
+const BOARD_WIDTH = 500
+const BOARD_HEIGHT = 400
+
+interface LinePops {
+  tool: string
+  points: number[]
+  stroke: string
+  strokeWidth: number
+}
+
+const ToolsPanel = (props) => {
+  const { setTool, setStrokeWidth, setStroke } = props
+  return (
+    <div>
+      <button value={"pen"} onClick={(e) => setTool(e.target.value)}>
+        Pen
+      </button>
+      <button value={"eraser"} onClick={(e) => setTool(e.target.value)}>
+        Eraser
+      </button>
+      <input
+        type="text"
+        name="strokeSize"
+        placeholder="size"
+        onChange={(e) => {
+          setStrokeWidth(parseInt(e.target.value))
+        }}
+      />
+      <input
+        type="text"
+        name="stroke"
+        placeholder="stroke"
+        onChange={(e) => {
+          setStroke(e.target.value)
+        }}
+      />
+    </div>
+  )
+}
+
 const KonvaBoard = () => {
-  const [tool, setTool] = useState<any>("pen")
-  const [lines, setLines] = useState<any>([])
+  const [tool, setTool] = useState<string>("pen")
+  const [lines, setLines] = useState<LinePops[]>([])
   const isDrawing = useRef(false)
+  const [strokeWidth, setSrokeWidth] = useState<number>(5)
+  const [stroke, setStroke] = useState<string>("#df4b26")
 
   const handleMouseDown = (e) => {
     isDrawing.current = true
     const pos = e.target.getStage().getPointerPosition()
-    setLines([...lines, { tool, points: [pos.x, pos.y] }])
+    setLines([...lines, { tool, points: [pos.x, pos.y], stroke, strokeWidth }])
   }
 
   const handleMouseMove = (e) => {
@@ -21,11 +63,14 @@ const KonvaBoard = () => {
     const stage = e.target.getStage()
     const point = stage.getPointerPosition()
     let lastLine = lines[lines.length - 1]
+
     // add point
-    lastLine.points = lastLine.points.concat([point.x, point.y])
+    //********* FIX IT */
+    // temporarry fix with !
+    lastLine!.points = lastLine!.points.concat([point.x, point.y])
 
     // replace last
-    lines.splice(lines.length - 1, 1, lastLine)
+    lines.splice(lines.length - 1, 1, lastLine!)
     setLines(lines.concat())
   }
 
@@ -36,8 +81,8 @@ const KonvaBoard = () => {
   return (
     <div className={styles.container}>
       <Stage
-        width={window.innerWidth}
-        height={window.innerWidth}
+        width={BOARD_WIDTH}
+        height={BOARD_HEIGHT}
         onMouseDown={handleMouseDown}
         onMousemove={handleMouseMove}
         onMouseup={handleMouseUp}
@@ -49,8 +94,8 @@ const KonvaBoard = () => {
             <Line
               key={i}
               points={line.points}
-              stroke="#df4b26"
-              strokeWidth={5}
+              stroke={line.stroke}
+              strokeWidth={line.strokeWidth}
               tension={0.5}
               lineCap="round"
               globalCompositeOperation={line.tool === "eraser" ? "destination-out" : "source-over"}
@@ -58,6 +103,8 @@ const KonvaBoard = () => {
           ))}
         </Layer>
       </Stage>
+      <ToolsPanel setTool={setTool} setStrokeWidth={setSrokeWidth} setStroke={setStroke} />
+
       <select
         value={tool}
         onChange={(e) => {
