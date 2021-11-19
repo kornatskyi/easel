571ffacc09279f1d7af89e1ced5d15ce480ee376
Bohdan <bohdan.kornatskyi@gmail.com>
2021-11-18 20:02:06 -0800

    basic undo redo

--- app/canvas/components/ToolsPanel.tsx
+++ app/canvas/components/ToolsPanel.tsx
@@ -3,13 +3,15 @@ import Tool from "./Tool"
 import Range from "./Range"
 import { ColorPicker } from "./ColorPicker"
 const ToolsPanel = (props) => {
-  const { setTool, setStrokeWidth, setStroke, strokeWidth, stroke } = props
+  const { setTool, setStrokeWidth, setStroke, strokeWidth, stroke, undo, redo } = props
 
   return (
     <div className={styles.container}>
       <div className={styles.buttons}>
         <Tool toolName={"pencil"} setTool={setTool} />
         <Tool toolName={"eraser"} setTool={setTool} />
+        <Tool toolName={"undo"} setTool={undo} />
+        <Tool toolName={"redo"} setTool={redo} />
       </div>
       <div className={styles.settings}>
         <Range value={strokeWidth} setStrokeWidth={setStrokeWidth} />
