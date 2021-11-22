fd4bb9ee3f0e1a86420dc9fc02e64a90ac8207b0
Bohdan <bohdan.kornatskyi@gmail.com>
2021-11-21 22:51:25 -0800

    styling

--- app/pages/canvas/index.tsx
+++ app/pages/canvas/index.tsx
@@ -1,7 +1,7 @@
 import React, { useRef, useState } from "react"
 import KonvaBoard from "app/canvas/components/KonvaBoard"
 import styles from "../../canvas/styles/canvaspage.module.scss"
-import { Image, Routes, useMutation, useRouter } from "blitz"
+import { Image, Link, Routes, useMutation, useRouter } from "blitz"
 import { FORM_ERROR } from "final-form"
 import createPost from "app/posts/mutations/createPost"
 import Form from "app/core/components/Form"
@@ -32,21 +32,33 @@ function CanvasPage(props) {
   }
 
   return (
-    <div className={styles.canvasPage}>
-      <h1>Canvas page</h1>
-      <Form
-        {...props}
-        onSubmit={(values) => {
-          handlePublish({ tags: values.tags, image: exportedImage })
-        }}
-      >
-        <LabeledTextField name="tags" label="Tags" placeholder="tags" />
-
-        <KonvaBoard publishButton={publishButton} setExportedImage={setExportedImage} />
-        <button ref={publishButton} className={styles.publishButton}>
-          Publish
-        </button>
-      </Form>
+    <div className="columns is-centered mgt-medium ">
+      <div className="column is-three-quarters content is-centered">
+        <div className="columns is-centered mt-4 is-justify-content-space-between px-6  is-flex	">
+          <Link href={Routes.Home()}>
+            <a className="is-align-self-center is-size-4">Home</a>
+          </Link>
+          <b className="has-text-primary is-size-1 is-align-self-center	">Canvas</b>
+          <Link href={Routes.PostsPage()}>
+            <a className="is-align-self-center		is-size-4">Posts</a>
+          </Link>
+        </div>
+        <Form
+          {...props}
+          onSubmit={(values) => {
+            handlePublish({ tags: values.tags, image: exportedImage })
+          }}
+        >
+          <KonvaBoard publishButton={publishButton} setExportedImage={setExportedImage} />
+          <div className="columns">
+            <LabeledTextField label="Tags" name="tags" placeholder="Tags" />
+            <LabeledTextField label="Title" name="title" placeholder="Title" />
+          </div>
+          <button ref={publishButton} className="button is-primary">
+            Publish
+          </button>
+        </Form>
+      </div>
     </div>
   )
 }
